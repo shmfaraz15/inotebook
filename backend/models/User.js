fda5f03aab8a04ec5ce1b1a4b8925645c43869df
faraz <shm.faraz@15gmail.com>
2023-01-21 16:15:59 +0530

    Navbar and Routes

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    name: {
+    name:{
         type: String,
         required: true
     },
-    email: {
+    email:{
         type: String,
         required: true,
         unique: true
     },
-    password: {
+    password:{
         type: String,
         required: true
     },
-    date: {
+    date:{
         type: Date,
         default: Date.now
     },
-});
-
-module.exports = mongoose.model('user', UserSchema);
+  });
+  const User = mongoose.model('user', UserSchema);
+  module.exports = User;
